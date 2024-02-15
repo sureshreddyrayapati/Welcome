b7e49956b87e1482d3289a9331218152c03963d2
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-15 12:45:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Welcome;
+Console.WriteLine("welcome to Authentication app");
+SigIn obj=new SigIn();
+Console.WriteLine(obj.Authentication("Suresh","Suresh@123"));
---
+++ SigIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome
+{
+    public class SigIn
+    {
+        public string Authentication(string username, string password)
+        {
+            string msg = "";
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "UserName and password reqired";
+            }
+            else
+            {
+                if (username == "Suresh" && password == "Suresh@123")
+                {
+                    msg = "Authendtication success";
+                }
+                else
+                {
+                    msg = "Authentication failed";
+                }
+            }
+            return msg;
+        }
+    }
+}
